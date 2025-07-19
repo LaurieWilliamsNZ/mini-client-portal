78f08c3207b49673dd79d60343710468bd26c53c
Laurie Williams <enquiries@lauriewilliamsdeveloper.com>
2025-07-19 14:12:20 +1200

    - add jestt setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,19 +1,8 @@
-// Mock react-native-gesture-handler
-// jest.mock('react-native-gesture-handler/jestSetup', () => {});
 
-// Mock react-native-reanimated
-// jest.mock('react-native-reanimated', () => {
-//   const Reanimated = require('react-native-reanimated/mock');
-//   Reanimated.default.call = () => {};
-//   return Reanimated;
-// });
-
-// Mock react-native-vector-icons
 jest.mock('react-native-vector-icons/MaterialCommunityIcons', () => 'Icon');
 
-// Mock react-native-svg
+
 jest.mock('react-native-svg', () => {
-  const React = require('react');
   const { View } = require('react-native');
   return {
     Svg: View,
@@ -32,13 +21,11 @@ jest.mock('expo-status-bar', () => ({
   StatusBar: 'StatusBar',
 }));
 
-// Mock react-native-safe-area-context
 jest.mock('react-native-safe-area-context', () => ({
   SafeAreaProvider: ({ children }) => children,
   useSafeAreaInsets: () => ({ top: 0, right: 0, bottom: 0, left: 0 }),
 }));
 
-// Mock react-native-paper
 jest.mock('react-native-paper', () => {
   const React = require('react');
   const { View, Text, TouchableOpacity } = require('react-native');
@@ -58,10 +45,7 @@ jest.mock('react-native-paper', () => {
   };
 });
 
-// Silence the warning: Animated: `useNativeDriver` is not supported
-// jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
-// Mock console methods to reduce noise in tests
 global.console = {
   ...console,
   log: jest.fn(),
